6830befee20c91d34cba33a8251c180adab5fcbf
Keith <keithwhisper@gmail.com>
2018-11-21 19:15:12 +0800

    update: save log locally

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const Koa = require('koa')
+const http = require('http')
 const AppChain = require("@appchain/base").default
 const {
-  randomTx
+  randomTx,
+  logTx,
 } = require('./utils')
 
 require('dotenv').config()
 
-const app = new Koa()
+const app = http.createServer()
+
 const appchain = AppChain(process.env.CHAIN)
 const account = appchain.base.accounts.privateKeyToAccount(process.env.PRIVATE_KEY)
 
@@ -14,38 +16,61 @@ appchain.base.accounts.wallet.add(account)
 
 const PORT = process.env.PORT || 3000
 
-setInterval(() => {
-  const rTx = randomTx()
-  let tx = { ...rTx
-  }
-  appchain.base.getBlockNumber().then(blockNumber => {
+const startTest = (interval) => {
+  setInterval(() => {
+    const rTx = randomTx()
+    let tx = { ...rTx,
+    }
+    const sender = appchain.base.accounts.wallet[0]
+    appchain.base.getBlockNumber().then(blockNumber => {
       tx = { ...tx,
         validUntilBlock: +blockNumber + 88,
         data: appchain.utils.fromUtf8(rTx.data),
         to: appchain.base.accounts.create().address
       }
-      return appchain.base.sendTransaction(tx)
-    }).then(txRes => {
-      if (txRes.status === 'OK') {
-        return appchain.listeners.listenToTransactionReceipt(txRes.hash)
-      } else {
-        throw new Error("Send tx failed")
-      }
-    })
-    .then(receipt => {
-      if (receipt.errorMessage) {
+      const signedMsg = appchain.base.signer(tx, process.env.PRIVATE_KEY)
+      try {
+        appchain.base.unsigner(signedMsg)
+      } catch (err) {
         const log = {
           tx,
-          errorMessage: receipt.errorMessage,
-          sender: appchain.base.accounts.wallet[0]
+          sender: {
+            privateKey: sender.privateKey,
+            address: sender.address,
+          },
+          errorMessage: err.toString()
         }
-        console.log(log)
+        return
       }
+      return appchain.base.sendTransaction(tx)
+        .then(txRes => {
+          if (txRes.status === 'OK') {
+            return appchain.listeners.listenToTransactionReceipt(txRes.hash)
+          } else {
+            throw new Error("Send tx failed")
+          }
+        })
+        .then(receipt => {
+          if (receipt.errorMessage) {
+            const log = {
+              tx,
+              sender: {
+                privateKey: sender.privateKey,
+                address: sender.address,
+              },
+              errorMessage: receipt.errorMessage,
+            }
+            logTx(log)
+          }
+        })
+        .catch(err => {
+          console.log(err)
+        })
     })
-    .catch(err => {
-      console.log(err)
-    })
-}, process.env.INTERVAL || 1000)
+  }, interval)
+}
+
+startTest(process.env.INTERVAL || 10000)
 
 app.listen(PORT, () => {
   console.log(`Sig Test Gen is running at ${PORT}`)
--- utils.js
+++ utils.js
@@ -1,21 +1,29 @@
+const fsPromise = require('fs').promises
+const path = require('path')
 const randomWords = require('random-words')
 const {
   tx
 } = require('./tpl')
-const randomTx = () => {
 
-  return {
-    ...tx,
-    quota: Math.round(Math.random() * 10000000),
-    nocne: Math.random(),
-    value: Math.round(Math.random() * 10000),
-    data: randomWords({
-      min: 10,
-      max: 100,
-      join: ' '
-    })
-  }
+const randomTx = () => ({
+  ...tx,
+  quota: Math.round(Math.random() * 10000000),
+  nocne: Math.random(),
+  value: Math.round(Math.random() * 10000),
+  data: randomWords({
+    min: 10,
+    max: 100,
+    join: ' '
+  })
+})
+
+
+
+const txLogFile = path.join(__dirname, './logs/txs.log')
+const logTx = (log) => {
+  return fsPromise.appendFile(txLogFile, JSON.stringify(log) + '\n')
 }
 module.exports = {
-  randomTx
+  randomTx,
+  logTx
 }
